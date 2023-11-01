f4c84eedde33e6b8f82b6a793450d6412ebd6e3b
vkclsys <vk2002clay@gmail.com>
2023-11-01 10:23:51 +0530

    sample_modified

--- src/sample.py
+++ src/sample.py
@@ -36,22 +36,19 @@ class Sample:
 
     @staticmethod
     def getFaceBox(net, frame, conf_threshold=0.7):
-        frameOpencvDnn = frame.copy()  # create a copy of the original frame
-        frameHeight = frameOpencvDnn.shape[0]  # get the height of the frame
-        frameWidth = frameOpencvDnn.shape[1]  # get the width of the frame
+        frameOpencvDnn = frame.copy()  
+        frameHeight = frameOpencvDnn.shape[0]  
+        frameWidth = frameOpencvDnn.shape[1] 
 
-        # creating a blob from the input image. Here, we pass in the frame having original size, scaling ratio of 1.0,
-        # output shape of the blob is 300x300, BGR mean values to subtract from the image, BRSwap to swap the B and R
-        # color channels is set to True and lastly cropping is set to False
         blob = cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
 
-        net.setInput(blob)  # pass the input image through the FaceNet, blob is of shape (1, 3, 300, 300)
-        detections = net.forward()  # apply forward pass
-        bboxes = []  # create empty bounding box list
+        net.setInput(blob)  
+        detections = net.forward() 
+        bboxes = []  
         for i in range(detections.shape[2]):
-            confidence = detections[0, 0, i, 2]  # get the bounding box confidence level
+            confidence = detections[0, 0, i, 2] 
 
-            # if the confidence is > 0.7 then get the top left and bottom right (x,y) co-ordinates
+           
             if confidence > conf_threshold:
                 x1 = int(detections[0, 0, i, 3] * frameWidth)  # top left x co-ordinate
                 y1 = int(detections[0, 0, i, 4] * frameHeight)  # top left y co-ordinate
@@ -59,70 +56,68 @@ class Sample:
                 y2 = int(detections[0, 0, i, 6] * frameHeight)  # bottom right y co-ordinate
                 bboxes.append([x1, y1, x2, y2])  # append the co-ordinates list computed above in the bounding box list
 
-                # draw a rectangle around the face and return the original image along with bounding box co-ordinates
+                
                 cv2.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight / 150)), 8)
 
         return frameOpencvDnn, bboxes
 
     def caffeInference(self):
 
-        # Open a video file or an image file or a camera stream
+        
         cap = cv2.VideoCapture(self.args.input if self.args.input else 0)
-        padding = 20  # padding the bounding box by 20 pixels on all sides
+        padding = 20 
 
-        # loop while any key press
+       
         while True:
-            # Read frame
-            t = time.time()  # start time for inference
-            hasFrame, frame = cap.read()  # get the image frame from the capture object of the camera
+            
+            t = time.time()  
+            hasFrame, frame = cap.read()  
 
-            # if no frame exists, then break out of the loop and stop inference
+            
             if not hasFrame:
                 # cv2.waitKey()
                 break
 
-            # pass the input frame along with face net model for getting bounding box information
+           
             frameFace, bboxes = self.getFaceBox(self.faceNet, frame)
 
-            # if the bounding box list is empty, just display empty frames, with a message of no face detected
+          
             if not bboxes:
                 print("No face Detected, Checking next frame")
                 cv2.putText(frameFace, "NO FACE DETECTED!", (40, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2,
-                            cv2.LINE_AA)  # render a message on the blank frame with no face
+                            cv2.LINE_AA)  
                 # cv2.imshow("Age Gender Demo", frameFace)  # display empty frames with message
             else:
-                # loop over all the bounding box detections if they exist
+            
                 for bbox in bboxes:
-                    # extend bounding box by 20 pixels on each side, essentially padding by 20 pixels taking into account
-                    # the factor that face may go out of frame. max and min functions are there to limit to the box dimensions
-                    # to the frame sizes
+                   
                     face = frame[max(0, bbox[1] - padding):min(bbox[3] + padding, frame.shape[0] - 1),
                            max(0, bbox[0] - padding):min(bbox[2] + padding, frame.shape[1] - 1)]
 
-                    # reshape the face image to 227x227 using the blob function and also swapping the RB planes
+                  
                     blob = cv2.dnn.blobFromImage(face, 1.0, (227, 227), self.MODEL_MEAN_VALUES, swapRB=False)
 
-                    # predict Gender
+                    
                     self.genderNet.setInput(
-                        blob)  # pass the 227x227 reshaped face blob to the gender net for prediction
-                    genderPreds = self.genderNet.forward()  # do the forward pass
-                    gender = self.genders[genderPreds[0].argmax()]  # get the gender
+                        blob) 
+                    genderPreds = self.genderNet.forward()  
+                    gender = self.genders[genderPreds[0].argmax()]  
 
-                    # print the gender along with its confidence score
+                  
                     print("Gender : {}, conf = {:.3f}".format(gender, genderPreds[0].max()))
+                  
 
-                    # predict Age
-                    self.ageNet.setInput(blob)  # pass the 227x227 face blob to the age net.
-                    agePreds = self.ageNet.forward()  # do forward pass
-                    age = self.ageList[agePreds[0].argmax()]  # get the age range
+                 
+                    self.ageNet.setInput(blob) 
+                    agePreds = self.ageNet.forward()  
+                    age = self.ageList[agePreds[0].argmax()] 
 
-                    # print the age along with its confidence score
                     print("Age : {}, conf = {:.3f}".format(age, agePreds[0].max()))
+                    
 
-                    # get the label to display on the image frame
+                    
                     label = "{},{}".format(gender, age)
 
-                    # place the text on the image with squared bounding box
                     cv2.putText(frameFace, label, (bbox[0], bbox[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255),
                                 2,
                                 cv2.LINE_AA)
@@ -133,20 +128,22 @@ class Sample:
                     # cv2.imshow("Age Gender Demo", frameFace)
 
             print("time : {:.3f}".format(time.time() - t))
+            print("Gender--> " ,gender )
+            print("Age--->   ", age)
 
     def torchInference(self):
 
-        # Open a video file or an image file or a camera stream
+        
         cap = cv2.VideoCapture(self.args.input if self.args.input else 0)
-        padding = 30  # padding the bounding box by 20 pixels on all sides
+        padding = 30  
 
-        # loop while any key press
+      
         while True:
             # Read frame
-            t = time.time()  # start time for inference
-            hasFrame, frame = cap.read()  # get the image frame from the capture object of the camera
+            t = time.time()  
+            hasFrame, frame = cap.read()  
 
-            # if no frame exists, then break out of the loop and stop inference
+            
             if not hasFrame:
                 # cv2.waitKey()
                 break
@@ -163,13 +160,11 @@ class Sample:
             else:
                 # loop over all the bounding box detections if they exist
                 for bbox in bboxes:
-                    # extend bounding box by 20 pixels on each side, essentially padding by 20 pixels taking into
-                    # account the factor that face may go out of frame. max and min functions are there to limit to
-                    # the box dimensions to the frame sizes
+                    
                     face = frame[max(0, bbox[1] - padding):min(bbox[3] + padding, frame.shape[0] - 1),
                            max(0, bbox[0] - padding):min(bbox[2] + padding, frame.shape[1] - 1)]
 
-                    # reshape the face image to 227x227 using the blob function and also swapping the RB planes
+                   
                     blob = cv2.dnn.blobFromImage(face, 1.0, (227, 227), self.MODEL_MEAN_VALUES, swapRB=False)
 
                     # cv2.imshow("Face blob", frameFace)
