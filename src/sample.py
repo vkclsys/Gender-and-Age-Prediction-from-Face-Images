9b38839bc0dcd3b0baaff9a781e0a6661d76058b
vkclsys <vk2002clay@gmail.com>
2023-11-05 10:59:24 +0530

    update

--- src/sample.py
+++ src/sample.py
@@ -1,7 +1,7 @@
 import time, math, argparse, cv2, sys, torch
 import numpy as np
 import json
-
+import random
 import os
 import cv2
 
@@ -261,8 +261,6 @@ class Sample:
                 video_capture.release()
 
 
-
-
             print("Gender--> " ,l_gender )
 
             print("Age--->   ", age)
@@ -274,6 +272,156 @@ class Sample:
 
         print("JSON file 'output.json' has been created.")
         
+    def extract_image_path(image_path):
+    
+        image_path = image_path.split('__frame')[0]
+
+        return image_path
+
+    def extract_video_path(video_path):
+        
+        base_name = os.path.basename(video_path)
+        return os.path.splitext(base_name)[0]
+    
+    def jsoncreation(self):
+
+        Jdata = []
+        swap_i = 1
+
+        for filename in os.listdir(self.args.input):
+            if filename.endswith((".mp4", ".mov")): 
+                video_path = os.path.join(self.args.input, filename)
+
+            male_count = 0
+            fmale_count = 0
+
+        
+            cap = cv2.VideoCapture(video_path if video_path else 0)
+            padding = 20 
+
+        
+            while True:
+                
+                
+                hasFrame, frame = cap.read()  
+
+                
+                if not hasFrame:
+                    break
+
+            
+                frameFace, bboxes = self.getFaceBox(self.faceNet, frame)
+
+            
+                if not bboxes:
+                    print("No face Detected, Checking next frame")
+                    cv2.putText(frameFace, "NO FACE DETECTED!", (40, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2,
+                                cv2.LINE_AA)  
+                    
+                else:
+                
+                    for bbox in bboxes:
+                    
+                        face = frame[max(0, bbox[1] - padding):min(bbox[3] + padding, frame.shape[0] - 1),
+                            max(0, bbox[0] - padding):min(bbox[2] + padding, frame.shape[1] - 1)]
+
+                    
+                        blob = cv2.dnn.blobFromImage(face, 1.0, (227, 227), self.MODEL_MEAN_VALUES, swapRB=False)
+
+                        
+                        self.genderNet.setInput(blob) 
+                        genderPreds = self.genderNet.forward()  
+                        gender = self.genders[genderPreds[0].argmax()]  
+
+                    
+                        
+                        if gender == "Male":
+                            male_count = male_count + 1
+
+                        if gender == "Female":
+                            fmale_count = fmale_count + 1
+
+                        self.ageNet.setInput(blob) 
+                        agePreds = self.ageNet.forward()  
+                        age = self.ageList[agePreds[0].argmax()] 
+
+
+
+
+            if male_count > fmale_count:
+
+                l_gender = "Male"
+
+
+                with open(self.args.Json_output, 'r') as file:
+                    data = json.load
+
+                matching_images = [item['Refernece_img'] for item in data if item['Gender'] == l_gender]
+
+
+                if not matching_images:
+                    return "No matching images found for the specified gender."
+
+                # Check if the video_path matches any of the image paths
+                
+                random.shuffle(matching_images)
+            
+                for image_path in matching_images:
+                    if self.extract_image_path(image_path) != self.extract_video_path(video_path):
+                        item_1 = {
+                                    "reference_video" : video_path,
+                                    "Refernece_img": image_path,
+                                    "output_video": f"swapped_video_{swap_i}.mp4",
+                                    "Gender":l_gender,
+                                    "Age":age
+                        }
+                        Jdata.append(item_1)
+                        swap_i = swap_i + 1
+                        break
+                    
+                    
+                        
+
+                
+            if fmale_count > male_count:
+
+                l_gender = "Female"
+
+
+                with open(self.args.Json_output, 'r') as file:
+                    data = json.load
+
+                matching_images = [item['Refernece_img'] for item in data if item['Gender'] == l_gender]
+
+
+                if not matching_images:
+                    return "No matching images found for the specified gender."
+
+                # Check if the video_path matches any of the image paths
+                
+                random.shuffle(matching_images)
+            
+                for image_path in matching_images:
+                    if self.extract_image_path(image_path) != self.extract_video_path(video_path):
+                        item_1 = {
+                                    "reference_video" : video_path,
+                                    "Refernece_img": image_path,
+                                    "output_video": f"swapped_video_{swap_i}.mp4",
+                                    "Gender":l_gender,
+                                    "Age":age
+                        }
+                        Jdata.append(item_1)
+                        swap_i = swap_i + 1
+                        break
+
+
+        with open(self.args.Json_output_j, 'w') as json_file:
+            json.dump(Jdata, json_file, indent=4)
+
+        print("JSON file has been created.")
+
+
+
 
         
 parser = argparse.ArgumentParser(description='Use this script to run age and gender recognition using OpenCV.')
@@ -285,7 +433,11 @@ parser.add_argument('-o', '--output', type=str, default="",
 parser.add_argument('-oj', '--Json_output', type=str, default="",
                     help='Path to output Json path.')
 
+parser.add_argument('-ojj', '--Json_output_j', type=str, default="",
+                    help='Path to output Json path.')
+
 args = parser.parse_args()
 s = Sample(args)
 s.caffeInference()
+s
 
